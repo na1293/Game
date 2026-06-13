fbb8075ff43db483ac433e14c251ecb7e6da4f8d
Namhai <namhai456n@gmail.com>
2026-06-13 22:03:28 +0700

    Thêm PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'hoc-app-v1';
+const ASSETS = [
+  './hoc.html',
+  './manifest.json'
+];
+
+// Cài đặt Service Worker và lưu tài nguyên vào bộ nhớ cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Kích hoạt và xóa cache cũ nếu có update
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Bắt các yêu cầu mạng để phản hồi từ cache nếu đang offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
