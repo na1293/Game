ac3071bbf3582fef87cdaff511c8f1cac271e961
Namhai <namhai456n@gmail.com>
2026-06-13 21:59:05 +0700

    Thêm học.html

---
+++ JS/check.js
@@ -0,0 +1,10 @@
+function main() {
+    const lua_chon = document.getElementById("lua-chon");
+    const main_lua_chon = lua_chon.value;
+
+    if (main_lua_chon == 1) {
+        rand()
+    } else {
+        tarrot_view()
+    }
+}
---
+++ JS/tarrot.js
@@ -0,0 +1,3 @@
+function tarrot_view() {
+    
+}
